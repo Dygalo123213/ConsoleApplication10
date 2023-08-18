aca7c6308036c345679933e4f00d9ecdb21e2bd8
Dygalo Yaroslav <VseTop1981@gmail.com>
2023-08-18 19:42:40 +0200

    defition staff cpp

--- ConsoleApplication10/staff.cpp
+++ ConsoleApplication10/staff.cpp
@@ -1,1 +1,23 @@
 #include "staff.h"
+
+staff::staff()
+{
+    bonus = 0;
+}
+
+staff::staff(string name, string position, float salary, float bonus)
+{
+    
+        this->bonus = bonus;
+}
+
+void staff::show() const
+{
+    
+    cout << "Bonus" << bonus << endl;
+}
+
+float staff::calcSalary() const
+{
+    return salary + bonus;
+}
