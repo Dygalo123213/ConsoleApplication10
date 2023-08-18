6d263d40101e4b079fde0b9f61510948faa15100
Dygalo Yaroslav <VseTop1981@gmail.com>
2023-08-18 19:17:48 +0200

    create class Employee

---
+++ ConsoleApplication10/Employee.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+class Employee
+{
+protected:
+	string name;
+	string position;
+	float salary;
+public:
+	Employee();
+	Employee(string name, string position, float salary);
+
+	virtual void show()const;
+	virtual float calcSalary()const;
+};
+
